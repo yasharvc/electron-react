6c0b2c06e655d599fc6bc2c0fc81c4676a9426b1
Yashar Aliabbasi <yashar.aliabbasi@patikaglobal.com>
2022-11-20 22:58:47 +0300

    Added basic react app and configured fileinterceptor

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, protocol } = require("electron");
 const path = require("path");
+const rootPath = require("electron-root-path").rootPath;
+const fs = require("fs");
 
 const createWindow = () => {
 	const win = new BrowserWindow({
@@ -9,12 +11,28 @@ const createWindow = () => {
 			preload: path.join(__dirname, "preload.js"),
 		},
 	});
-
-	win.loadFile("index.html");
+	const filePath = "public/index.html";
+	win.loadFile(filePath);
 };
 
 app.whenReady().then(() => {
 	createWindow();
+	protocol.interceptFileProtocol("file", (request, callback) => {
+		let newUrl = request.url
+			.replace("file:", "")
+			.replace("///", "")
+			.replace("//", "");
+		if (fs.existsSync(newUrl)) {
+			console.log(`Without change:${newUrl}`);
+			callback(newUrl);
+		} else {
+			if (newUrl[1] == ":")
+				newUrl = newUrl.substring(newUrl.indexOf(":") + 1);
+			newUrl = `${rootPath}/public${newUrl}`;
+			console.log("New Url" + newUrl);
+			callback(newUrl);
+		}
+	});
 });
 
 app.on("window-all-closed", () => {
